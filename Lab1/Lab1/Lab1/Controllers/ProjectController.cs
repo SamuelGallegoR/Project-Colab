b1b2444b34a5edaaef046a6dff2b790be8194fd1
SamuelGallego <samuelgallego18@gmail.com>
2024-01-17 21:50:34 -0500

    Second Commit - Lab 2

---
+++ Lab1/Lab1/Lab1/Controllers/ProjectController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace Lab1.Controllers
+{
+    public class ProjectController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var projects = new List<Project>()
+            {
+                new Project {projectID = 1, Name = "Project 1", Description = "My first project"}
+
+            };
+
+               return View(projects);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+    } 
+}
+
---
+++ Lab1/Lab1/Lab1/Models/Project.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab1.Models
+{
+	public class Project
+	{
+		//Attributes
+
+		public int projectID { get; set; } //This creates a PRIVATE attribute but public getter and setter for the attribute
+
+		[Required] //"Required" annotation
+		public string Name { get; set; }
+
+		public string ? Description { get; set; } //the question mark mean this field is nullable (optional) it can be a string or Null.
+
+		[DataType(DataType.Date)]		//This is called an annotation to enforce a certian datatype
+		public DateTime StartDate { get; set; }
+
+        [DataType(DataType.Date)]		
+        public DateTime EndDate { get; set; }
+
+		public string? Status { get; set; }
+
+
+
+        public Project()
+		{
+		}
+	}
+}
+
